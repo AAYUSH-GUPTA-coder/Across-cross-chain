967c3a439c94228b4f7a8241e9852a24609fd35a
Aayush Gupta <guptaaayush432@gmail.com>
2024-03-13 16:20:42 +0530

    removed extra code

--- src/Destination.sol
+++ src/Destination.sol
@@ -10,20 +10,9 @@ error Unauthorized();
 contract Destination {
     InterfaceV3SpokePool public spokePool;
     address public immutable acrossSpokePool;
-    // ISwapRouter public router;
-    // address public immutable wethAddr;
-    // address public immutable wstEthAddr;
 
-    constructor(
-        // address _router,
-        address _acrossSpokePool
-        // address _wethAddr,
-        // address _wstEthAddr
-    ) {
-        // router = ISwapRouter(_router);
+    constructor(address _acrossSpokePool) {
         acrossSpokePool = _acrossSpokePool;
-        // wethAddr = _wethAddr;
-        // wstEthAddr = _wstEthAddr;
     }
 
     function handleV3AcrossMessage(
@@ -34,41 +23,5 @@ contract Destination {
     ) external {
         // Verify that this call came from the Across SpokePool.
         if (msg.sender != acrossSpokePool) revert Unauthorized();
-
-        // Decodes the user address from the message.
-        // address user = abi.decode(message, (address));
-
-        // swapExactInputSingleHop(
-        //     wethAddr,
-        //     wstEthAddr,
-        //     100,
-        //     amount,
-        //     address(this)
-        // );
     }
-
-    // function swapExactInputSingleHop(
-    //     address tokenIn,
-    //     address tokenOut,
-    //     uint24 poolFee,
-    //     uint amountIn,
-    //     address receiver
-    // ) public returns (uint amountOut) {
-    //     IERC20(tokenIn).approve(address(router), amountIn);
-
-    //     ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-    //         .ExactInputSingleParams({
-    //             tokenIn: tokenIn,
-    //             tokenOut: tokenOut,
-    //             fee: poolFee,
-    //             // recipient: address(this),
-    //             recipient: receiver,
-    //             deadline: block.timestamp,
-    //             amountIn: amountIn,
-    //             amountOutMinimum: 0, // need to be calcuated off-chain and need to take the value in parameter
-    //             sqrtPriceLimitX96: 0
-    //         });
-
-    //     amountOut = router.exactInputSingle(params);
-    // }
 }
